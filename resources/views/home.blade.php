18596b7dba5dea52a76f459c8135c643d8eadc07
alexandrupuscas <alexandru.puscas@clever-software-solutions.com>
2017-05-24 20:34:50 +0300

    Added upload image feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,35 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+    <div class="container spark-screen">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div>
+                        <img src="/images/header.png" width="100%"/>
+                    </div>
+                    <div class="panel-body">
+                        <form method="POST" action="/upload" enctype="multipart/form-data">
+                            @foreach ($errors->all() as $error)
+                                <p class="alert alert-danger">{{ $error }}</p>
+                            @endforeach
+                            @if (session('status'))
+                                <div class="alert alert-success">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            {!! csrf_field() !!}
+                            <div class="form-group">
+                                <label for="image">Choose an image</label>
+                                <input type="file" id="image" name="image">
+                            </div>
+                            <button type="submit" class="btn btn-default">
+                                Upload
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
