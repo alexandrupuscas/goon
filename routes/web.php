3c0a048d71c8f25f964c2bf6ee1c69eee6de6439
alexandrupuscas <alexandru.puscas@clever-software-solutions.com>
2017-05-31 20:18:16 +0300

    Created API endpoints

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::post('upload', 'ImagesController@store');
 
 Route::get('/blog', 'BlogController@index');
 
+Route::group(['prefix' => 'api/v1', 'middleware' => 'api'], function(){
+    Route::resource('posts', 'PostsController');
+});
+
 //Route::get('json', function () {
 //    return App\Post::paginate();
 //});
